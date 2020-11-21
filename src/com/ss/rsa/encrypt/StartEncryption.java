f14d97153b1bb48f1b96a6019eba18767c0ee793
Udeesha Bandara <udeeshabandara@gmail.com>
2020-11-21 09:28:14 +0530

    Key change

--- src/com/ss/rsa/encrypt/StartEncryption.java
+++ src/com/ss/rsa/encrypt/StartEncryption.java
@@ -13,16 +13,16 @@ public class StartEncryption {
 	
 	public static void main(String[] args) throws IOException, GeneralSecurityException, Exception {
 		// TODO Auto-generated method stub
-		KeyHandling startEnc = new KeyHandling();
+		KeyHandling key = new KeyHandling();
 	
 		
 		File originalFile = new File(FileHandling.clearTextFile);
 		File encryptedFile = new File(FileHandling.baseLocationRSA+"EncryptedFiles/encryptedFile.txt");
-		new EncryptData(originalFile, encryptedFile, startEnc.getSecretKey(FileHandling.baseLocationRSA+"AES/AESKey.txt", "AES"), "AES");
+		new EncryptData(originalFile, encryptedFile, key.getSecretKey(FileHandling.baseLocationRSA+"AES/AESKey.txt", "AES"), "AES");
 	
 		File originalKeyFile = new File(FileHandling.baseLocationRSA+"AES/AESKey.txt");
 		File encryptedKeyFile = new File(FileHandling.baseLocationRSA+"EncryptedFiles/encryptedAESKey.txt");
-		new EncryptKey(startEnc.getPublic(FileHandling.baseLocationRSA+"RSA_Keys/receiverPublicKey.txt", "RSA"), originalKeyFile, encryptedKeyFile, "RSA");
+		new EncryptKey(key.getPublic(FileHandling.baseLocationRSA+"RSA_Keys/receiverPublicKey.txt", "RSA"), originalKeyFile, encryptedKeyFile, "RSA");
 	
 
 	}
